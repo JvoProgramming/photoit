2bbeeac66ae913c0a0f4a7ffc8d4a0ab79a4cde5
Johnny Vo <jvoprogramming@gmail.com>
2025-02-12 01:23:08 -0800

    fixed backend

--- server/posts.js
+++ server/posts.js
@@ -4,6 +4,27 @@ var firebase = require('firebase/app');
   require('firebase/database');
 
 
+function sortByProperty(objArray, prop, direction){
+  if (arguments.length<2) throw new Error("ARRAY, AND OBJECT PROPERTY MINIMUM ARGUMENTS, OPTIONAL DIRECTION");
+  if (!Array.isArray(objArray)) throw new Error("FIRST ARGUMENT NOT AN ARRAY");
+  const clone = objArray.slice(0);
+  const direct = arguments.length>2 ? arguments[2] : 1; //Default to ascending
+  const propPath = (prop.constructor===Array) ? prop : prop.split(".");
+  clone.sort(function(a,b){
+      for (let p in propPath){
+              if (a[propPath[p]] && b[propPath[p]]){
+                  a = a[propPath[p]];
+                  b = b[propPath[p]];
+              }
+      }
+      // convert numeric strings to integers
+      a = a.match(/^\d+$/) ? +a : a;
+      b = b.match(/^\d+$/) ? +b : b;
+      return ( (a < b) ? -1*direct : ((a > b) ? 1*direct : 0) );
+  });
+  return clone;
+}
+
 //////////////////////////////////////////////////////////////////////// get functions
 
 function getPost(request, response) {
@@ -86,6 +107,8 @@ function getCategoryPosts(request, response) {
 
   var queryUsersPosts = firebase.database().ref("Posts").orderByChild("Category").equalTo(category);
 
+  console.log("queryUsersPosts: " + queryUsersPosts);
+
   queryUsersPosts.once('value', function(snapshot) {
 
     if (snapshot.numChildren() >= 1) {
@@ -105,7 +128,7 @@ function getCategoryPosts(request, response) {
         }
       }
       
-      filteredJSON = sorting.sortByProperty([filteredJSON], 'attributes.Likes', -1);
+      filteredJSON = sortByProperty([filteredJSON], 'attributes.Likes', -1);
 
       console.log("getCategoryPosts() - found posts");
       response.send(filteredJSON);
